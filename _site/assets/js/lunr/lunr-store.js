0b96322784c1d5317e6fe42308aeceaed1d72c05
Paresy a <paresya888@gmail.com>
2025-09-18 21:43:24 +0200

    Added TIPE

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Tipe",
-        "excerpt":" ","categories": [],
+        "excerpt":"This is a math write-up I made two years ago for the TIPE part of my competitive exams. It actually ended up being useless since Polytechnique replaces the TIPE with a scientific document analysis exam, which feels fairer since the quality of the TIPE can depend a lot on your...","categories": [],
         "tags": [],
         "url": "/TIPE/",
         "teaser": "/assets/Lu_Zhishen_Water_Margin_2.jpg"
