1c3bfb81b82eedb44a6c5b03365d3f3d8c0f91b7
aparesy <aparesy@users.noreply.github.com>
2026-07-27 07:47:43 +0000

    deploy: 8a2ab1a1b7e83bda779f15f8d619653aaf8ea56b

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,18 +1,18 @@
 var store = [{
-        "title": "Tipe",
+        "title": "My TIPE: the Gale-Stewart theorem",
         "excerpt":"This is a math write-up I made two years ago for the TIPE part of my competitive exams. It actually ended up being useless since Polytechnique replaces the TIPE with a scientific document analysis exam, which feels fairer since the quality of the TIPE can depend a lot on your...","categories": [],
         "tags": [],
         "url": "/TIPE/",
         "teaser": "/assets/Lu_Zhishen_Water_Margin_2.jpg"
       },{
         "title": "Beautiful algorithms",
-        "excerpt":"Introduction Not all algorithms are beautiful. I discovered this theorem while practicing on codeforces, which has a surprisingly large amount of problems involving one or multiple array and a very convoluted operation ; the perfect recipe for racking your brains during 30 minutes before finding a not-so-satisfying (and very forgettable)...","categories": [],
+        "excerpt":"Introduction Not all algorithms are beautiful. I discovered this theorem while practicing on Codeforces, which has a surprisingly large number of problems involving one or multiple arrays and a very convoluted operation; the perfect recipe for racking your brains for 30 minutes before finding a not-so-satisfying (and very forgettable) solution....","categories": [],
         "tags": [],
         "url": "/BeautifulAlgos/",
         "teaser": "/assets/Lu_Zhishen_Water_Margin_2.jpg"
       },{
         "title": "Lucky week",
-        "excerpt":"I spent a lot of time in the past 4 months thinking about competitive programming, half of which being spent lying down staring at the ceiling while contemplating and the other half actually practicing, but it seems that this practice method actually bear fruit last week through some events. Since...","categories": [],
+        "excerpt":"I spent a lot of time in the past 4 months thinking about competitive programming, half of which was spent lying down staring at the ceiling while contemplating and the other half actually practicing, but it seems that this practice method actually bore fruit last week through some events. Since...","categories": [],
         "tags": [],
         "url": "/Lucky_week/",
         "teaser": "/assets/Lu_Zhishen_Water_Margin_2.jpg"
